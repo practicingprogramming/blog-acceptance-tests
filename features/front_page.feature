fe9a29b3a7df8f48132bf657375cf60d54935c56
practicingprogramming <practicingprogramming@gmail.com>
2016-02-29 18:52:48 -0800

    Test clicking on a post

--- features/front_page.feature
+++ features/front_page.feature
@@ -4,3 +4,9 @@ Scenario: Open front page
 
   Given I am on the front page
   Then I should see posts
+
+Scenario: Click on a posts
+
+  Given I am on the front page
+  When I click on a post
+  Then I am taken to the post page
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -5,3 +5,11 @@ end
 Then /^I should see posts$/ do
   expect(page).to have_xpath('//ng-include[@src="post.partial"]')
 end
+
+When(/^I click on a post$/) do
+  first('h3 > a').click
+end
+
+Then(/^I am taken to the post page$/) do
+  expect(current_url).to match(/.*\post\/.+/)
+end
