2412f3b3e10dd2f813ed04e489c8b3a0bd45f4bf
practicingprogramming <practicingprogramming@gmail.com>
2016-02-29 18:44:42 -0800

    Support different stages

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,11 @@
-Capybara.app_host = 'http://practicingprogramming.com'
+case ENV['stage']
+when 'local'
+  Capybara.app_host = 'http://localhost:8080'
+when 'prod'
+  Capybara.app_host = 'http://practicingprogramming.com'
+else
+  fail 'Stage is unknown or not set'
+end
 
 case ENV['driver']
 when 'selenium'
